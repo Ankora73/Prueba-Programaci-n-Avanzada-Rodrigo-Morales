d26684f9b4c344156c875eb4ef29ae26af99563c
Ankora <rodrigomoralese91@gmail.com>
2020-05-31 12:25:28 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Prueba_Programación_Avanzada_Rodrigo_Morales
+{
+    class Program
+    {
+        private class auto
+        {
+            public int numChasis { get; set; }
+            public int cilindrada { get; set; }
+            public double precio { get; set; }
+            public string tipoAuto { get; set; }
+            public int tipoTraccion { get; set; }
+            public int version { get; set; }
+            
+        
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+/*
+ Desde la clase que contiene el main debe:
+• Agregar un Auto Suv y un Auto Sedán a la colección.
+• Debe mostrar el total final para el Auto Suv.
+• Obtener la cantidad de Autos caros.
+• Eliminar un Auto
+ */
+
+/*
+ Validar todas las opciones en el despliegue tipo menú o similar
+* Uso de Colecciones obligatorio, al menos 1.
+ */
